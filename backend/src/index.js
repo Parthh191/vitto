1a5563328fd288b584343c3767d454e4384b8b9c
Wizard <wizard@coderwizard.local>
2026-06-09 12:47:29 +0530

    update the cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,23 +2,34 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import applicationRoutes from "./routes/applications.route.js";
+
 dotenv.config();
+
 const app = express();
-const FRONTEND_URL = process.env.FRONTEND_URL ;
-if(!FRONTEND_URL) {
-    console.error("FRONTEND_URL is not defined in .env file");
-    process.exit(1);
-}
-console.log("FRONTEND_URL:", FRONTEND_URL);
-app.use(cors({
-    origin: FRONTEND_URL,
-    methods: ['GET', 'POST', 'PATCH'],
-}));
+
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
+
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-// 404 handler
+
+app.use((req, res, next) => {
+  console.log("Origin:", req.headers.origin);
+  next();
+});
+
 app.use("/applications", applicationRoutes);
+
 app.use((req, res) => {
-  res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({
+    error: "Route not found",
+  });
 });
 
 export default app;
